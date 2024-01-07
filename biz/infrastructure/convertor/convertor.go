755854e8facfe601e7d370ce19b474260c946e43
Lansong <2095621390@qq.com>
2024-01-07 21:31:33 +0800

    fix: 修复bug

--- biz/infrastructure/convertor/convertor.go
+++ biz/infrastructure/convertor/convertor.go
@@ -27,7 +27,7 @@ func UserDetailToUserMapper(in *gencontent.UserDetailInfo) *usermapper.User {
 	return &usermapper.User{
 		ID:          ID,
 		Name:        in.Name,
-		Sex:         in.GetSex(),
+		Sex:         int32(in.GetSex()),
 		FullName:    in.FullName,
 		IdCard:      in.IdCard,
 		Description: in.Description,
